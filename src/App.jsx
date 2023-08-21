66a7e1886d88cae075494635524afabaa2f2ace7
Mark Soro <napoleon_soro@rocketmail.com>
2023-08-21 13:14:47 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./app.css";
+import { useState, useEffect } from "react";
+import TodoList from "./components/TodoList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
